d7a79be6464cf094e743991788aa12ebcf9cebee
gaurav <98477295+solarpunkin@users.noreply.github.com>
2025-09-03 23:34:17 +0530

    Delete wire_protocol.go

--- wire_protocol.go
+++
@@ -1,6 +0,0 @@
-| "FSND" (4 bytes) | ver: uint16 | flags: uint16 |
-| file_size: uint64 (network order) |
-| name_len: uint16 | filename (name_len bytes) |
-| file_data (file_size bytes) |
-| crc32: uint32 (network order) |
-| sha1: 20 bytes |
