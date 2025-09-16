0455e5b14b0bba2fe00903d0b28432a8b2b9f6b7
gaurav-bhardwaj29 <gaurav290802@gmail.com>
2025-09-17 00:06:31 +0530

    re:receiver file transfer over TCP & chunk based checksum

--- crc-32/receiver.c
+++ crc-32/receiver.c
@@ -4,24 +4,45 @@
 -- reports success/failure
 */
 
+#include <arpa/inet.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include "crc32.h"
+
+#define CHUNK 4096
+#define BACKLOG 8
 
 #define PORT 8080
 #define BUF_SIZE 1024
+
+static ssize_t readn(int fd, void *buf, size_t n) {
+    unsigned char *p = buf;
+    size_t left = n;
+    while (left) {
+        ssize_t r = recv(fd, p, left, 0);
+        if (r < 0) {
+            if (errno == EINTR) continue;
+            return -1;
+        }
+        if (r == 0) return (ssize_t)(n - left); // EOF
+        p += r;
+        left -= r;
+    }
+    return (ssize_t)n;
+}
 
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char buffer[BUF_SIZE];
-    size_t n;
-    long filesize, received = 0;
-    uint32_t recv_crc, calc_crc = 0;
+    uint64_t filesize;
+    uint32_t recv_crc;
     FILE *fp;
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -52,8 +73,8 @@ int main() {
     }
 
     // Receive file size
-    read(new_socket, &filesize, sizeof(filesize));
-    printf("Incoming file of size %ld bytes\n", filesize);
+    readn(new_socket, &filesize, sizeof(filesize));
+    printf("Incoming file of size %llu bytes\n", filesize);
 
     fp = fopen("received_file", "wb");
     if (!fp) {
@@ -61,22 +82,33 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    while (received < filesize) {
-        n = read(new_socket, buffer, BUF_SIZE);
-        if (n <= 0) break;
-        fwrite(buffer, 1, n, fp);
-        calc_crc = xcrc32((unsigned char *)buffer, n, calc_crc);
-        received += n;
+    uint32_t calc_crc = 0xFFFFFFFFu;
+    uint64_t left = filesize;
+    unsigned char buf[CHUNK];
+    while (left) {
+        size_t want = (left > CHUNK) ? CHUNK : (size_t)left;
+        ssize_t r = readn(new_socket, buf, want);
+        if (r <= 0) { perror("read data"); fclose(fp); close(new_socket); close(server_fd); exit(EXIT_FAILURE); }
+        if (fwrite(buf, 1, (size_t)r, fp) != (size_t)r) { perror("fwrite"); fclose(fp); close(new_socket); close(server_fd); exit(EXIT_FAILURE); }
+        calc_crc = xcrc32(buf, (int)r, calc_crc);
+        left -= (size_t)r;
     }
+    fflush(fp);
     fclose(fp);
 
     // Receive sender’s CRC32
-    read(new_socket, &recv_crc, sizeof(recv_crc));
+    if (readn(new_socket, &recv_crc, sizeof(recv_crc)) != sizeof(recv_crc)) {
+        perror("read crc");
+        close(new_socket);
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    uint32_t recv_crc_net = ntohl(recv_crc);
 
-    printf("Sender CRC32: %08X\n", recv_crc);
+    printf("Sender CRC32: %08X\n", recv_crc_net);
     printf("Calc   CRC32: %08X\n", calc_crc);
 
-    if (recv_crc == calc_crc)
+    if (recv_crc_net == calc_crc)
         printf("CRC32 matches!\n");
     else
         printf("CRC32 mismatch!\n");
